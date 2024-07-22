ca37a7dbc04a083ddab7504e666a4f8a1782bfb3
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-22 12:11:52 +0300

    feat: add NOBJ website

---
+++ joes/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+    return <div className={'w-full h-fit flex flex-col justify-center items-center'}>
+        <img className={"w-[40%]"} src={'nobj.jpg'} />
+        <img src={'resignation.jpg'} />
+    </div>
+}
+
+export default App
